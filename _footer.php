ae13844a525267d4fc483786916f8fa6f91600ef
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2020-06-06 08:17:15 +0700

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,7 @@
+	<footer>
+		<p>Copyright © 2020 | Group 4 Design by Rizqi Setiaji & Azhar Gunawan</p>
+	</footer>
+
+	<script src="<?=url('_assets/js/rumah_laundry.js')?>"></script>
+</body>
+</html>
---
+++ about.php
@@ -0,0 +1,154 @@
+<?php require_once('_header.php') ?>
+
+   <div class="video-btn">
+      <a href="#overlay">
+         <img src="<?=url('_assets/img/play.svg')?>" alt="play icon">
+         <p>Company Video</p>
+      </a>
+   </div>
+
+   <div class="overlay video-play" id="overlay"> 
+      <a href="#">&times;</a>
+      <div class="konten-video">
+      <iframe width="100%" height="500" src="https://www.youtube.com/embed/Yoemduj1qxM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+   </div>
+
+   <div id="about" class="main-content">
+      <div class="container">
+         <div class="baris">
+            <div class="col mt-2">
+               <div class="card">
+                  <div class="card-body">
+                     <div class="card-flex-column">
+                        <div class="about_header">
+                           <img src="<?=url('_assets/img/logo/logo.png')?>" alt="Logo rumah laundry" width="220">
+                           <p>Jasa laundry tercepat dan termurah se-priangan timur.</p>
+                        </div>
+                        <div class="about_us">
+                           <h2>- Tentang Kami -</h2>
+                           <p>Rumah Laundry UBSI merupakan suatu pelayanan di bidang jasa yang menggunakan media online sebagai Platform untuk memudahkan pelanggan menemukan jasa laundry yang cepat dan mudah. Sebagaimana kita ketahui bahwasannya kemudahan mendapatkan berbagai macam jasa pada era sekarang sangatlah diperlukan, selain mudah dan cepat, kami menyediakan berbagai macam paket laundry yang sesuai dengan budget.</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div class="baris">
+            <div class="col">
+               <div class="card">
+                  <div class="card-body">
+                     <div class="txt-center mt-3">
+                        <h2>- Members -</h2>
+                     </div>
+                     <div class="card-inner">
+                        <div class="col-members">
+                           <div class="card-member">
+                              <div class="profile-img">
+                                 <img src="<?=url('_assets/img/members/azhar.jpg')?>" alt="azhar profile">
+                              </div>
+                              <div class="desc-member">
+                                 <h4>Muhammad Azhar Gunawan</h4>
+                                 <small>Tasikmalaya, 08 November 2000</small>
+                                 <p>Leader/Front-End Developer</p>
+                                 <div class="social-media">
+                                    <a href="https://www.facebook.com/azhar.gunawan.92" target="_blank">
+                                       <img src="<?=url('_assets/img/facebook.png')?>" alt="facebook" width="60">
+                                    </a>
+                                    <a href="https://www.instagram.com/azhargunawan00/?hl=id" target="_blank">
+                                       <img src="<?=url('_assets/img/instagram.png')?>" alt="instagram" width="70">
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+
+                           <div class="card-member">
+                              <div class="profile-img">
+                                 <img src="<?=url('_assets/img/members/mayly.jpg')?>" alt="mayly profile">
+                              </div>
+                              <div class="desc-member">
+                                 <h4>Mayly Maytiekha</h4>
+                                 <small>Cilacap, 19 Maret 2002</small>
+                                 <p>Operasi Sistem</p>
+                                 <div class="social-media">
+                                    <a href="https://www.facebook.com/mayly.maytiekha.9" target="_blank">
+                                       <img src="<?=url('_assets/img/facebook.png')?>" alt="facebook" width="60">
+                                    </a>
+                                    <a href="https://www.instagram.com/maylymaytiekha19/?hl=id" target="_blank">
+                                       <img src="<?=url('_assets/img/instagram.png')?>" alt="instagram" width="70">
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+
+                        <div class="col-members">
+                           <div class="card-member">
+                              <div class="profile-img">
+                                 <img src="<?=url('_assets/img/members/baverly.jpg')?>" alt="baverly profile">
+                              </div>
+                              <div class="desc-member">
+                                 <h4>Baverly Ricardo Sella</h4>
+                                 <small>Tasikmalaya, 28 Mei 1999</small>
+                                 <p>Marketing</p>
+                                 <div class="social-media">
+                                    <a href="https://www.facebook.com/ricardo.sellaa" target="_blank">
+                                       <img src="<?=url('_assets/img/facebook.png')?>" alt="facebook" width="60">
+                                    </a>
+                                    <a href="https://www.instagram.com/beverly.rs/?hl=id" target="_blank">
+                                       <img src="<?=url('_assets/img/instagram.png')?>" alt="instagram" width="70">
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+
+                        <div class="col-members">
+                           <div class="card-member">
+                              <div class="profile-img">
+                                 <img src="<?=url('_assets/img/members/rizqi.jpg')?>" alt="rizqi profile">
+                              </div>
+                              <div class="desc-member">
+                                 <h4>Rizqi Setiaji</h4>
+                                 <small>Ciamis, 14 Juli 1998</small>
+                                 <p>Vice Leader/Back-End Developer</p>
+                                 <div class="social-media">
+                                    <a href="https://www.facebook.com/rizqi.setiaji" target="_blank">
+                                       <img src="<?=url('_assets/img/facebook.png')?>" alt="facebook" width="60">
+                                    </a>
+                                    <a href="https://www.instagram.com/rizqisetiaji7/" target="_blank">
+                                       <img src="<?=url('_assets/img/instagram.png')?>" alt="instagram" width="70">
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+
+                           <div class="card-member">
+                              <div class="profile-img">
+                                 <img src="<?=url('_assets/img/members/santi.jpg')?>" alt="santi profile">
+                              </div>
+                              <div class="desc-member">
+                                 <h4>Santi Herlina</h4>
+                                 <small>Tasikmalaya, 27 Mei 2000</small>
+                                 <p>Data Research</p>
+                                 <div class="social-media">
+                                    <a href="https://www.facebook.com/estii.kato" target="_blank">
+                                       <img src="<?=url('_assets/img/facebook.png')?>" alt="facebook" width="60">
+                                    </a>
+                                    <a href="https://www.instagram.com/santihrln/?hl=id" target="_blank">
+                                       <img src="<?=url('_assets/img/instagram.png')?>" alt="instagram" width="70">
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+
+<?php require_once('_footer.php') ?>
